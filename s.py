b2bd8bbb2fb880a067bd95e537eed7be0be1c1cb
matti <rev.matthewagilmore@gmail.com>
2017-02-22 22:03:20 -0600

    update

---
+++ s.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os,time
+	
+#os.system allows you to type
+os.system(" links http://www.google.com")
+'''
+time.sleep waits to do anything for the amount of secs you tell it to.
+time.sleep(10)
+it turns outthat the next  python  code well  not run time after links is closed.
+
+ps \r is enter i think
+'''
+
